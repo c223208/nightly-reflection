b04de6df1f57a15fddecae6f4c48dbb87b5bf47e
c223208 <c223208@ed.nagoya-cu.ac.jp>
2025-07-24 14:33:45 +0900

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,25 @@
+const CACHE_NAME = 'flask-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/home'
+];
+
+// インストール時にキャッシュ
 self.addEventListener('install', event => {
-  console.log('Service worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
+
+// フェッチ時にキャッシュを使う
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
